4c6dac2de03d70b749eeea39d214a702e94c6ea7
Dan Vincent Tagailo <danvincenttagailo@gmail.com>
2024-01-06 00:20:51 +0800

    Update scheduler.ts

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,129 +1,94 @@
+/**
+ * Author: Dan Vincent Tagailo
+ * Vendor: --
+ * Project: --
+ * Feature: Job Scheduler (No Processor)
+ */
 
-import {CronJob, cronJob} from '@loopback/cron';
+import { CronJob, cronJob, asCronJob } from '@loopback/cron';
+import { RestApplication } from '@loopback/rest';
+
+import DogController from './controllers/dog.controller';
+import CatController from './controllers/cat.controller';
+
+import ProcessChickenProvider from './services/processors/process-chicken.service';
+
 // import {CustomerInfoRepository} from '../repositories';
-import ProcessAgingDocProvider from './services/process-aging-doc.service';
-import ProcessForCorrectDocProvider from './services/process-forCorrect-doc.service';
-import ProcessLinkingDocProvider from './services/process-linking-doc.service';
-import ProcessTransmissionDocProvider from './services/process-transmission-doc.service';
-import ProcessUpdateDocProvider from './services/process-update-doc-service';
+
+import { bold, italic } from 'chalk';
+
+/**
+  * UNIX Cron format:
+  * https://github.com/kelektiv/node-cron#api
+  *   field          allowed values
+  *   -----          --------------
+  *   second         0-59
+  *   minute         0-59
+  *   hour           0-23
+  *   day of month   1-31
+  *   month          1-12 (or names, see below)
+  *   day of week    0-7 (0 or 7 is Sunday, or use names)
+  * 
+  * Other Ref: https://quadrixm.medium.com/running-a-cron-job-in-loopback-4-for-updating-a-database-table-using-a-loopback-repository-97fcf5c96e28
+  **/
 
 @cronJob()
 export default class Scheduler extends CronJob {
-  constructor(
-    // @repository(CustomerInfoRepository) public customerInfoRepository: CustomerInfoRepository,
-    name: string,
-    cronTime: string
-  ) {
+  constructor(name: string, cronTime: string, public app: RestApplication) {
+    // Do not insert code before super()
     super({
       name,
-      onTick: async () => {
-        await this.sendToProcessor();
+      onTick: () => {
+        console.log(italic.magentaBright('->', 'scheduler:constructor', '=> Calling sendToProcessor function...'))
+        this.sendToProcessor();
       },
-      cronTime,
+      cronTime, // Please always use UNIX Cron format (recommended: 6 ticks or fields )
       start: true,
     });
-    name = this.name
+    console.log(italic.magentaBright('->', 'scheduler:constructor', '=> Scheduler constructor is called'))
   }
 
-  async fetchRecordsToLink() {
+  public name: string;
+  public cronTime: string;
+  public job: CronJob;
+
+  // Call the controller
+  async invokeController(ctrlName, functionName) {
+    console.log(italic.magentaBright('->', `scheduler:invokeController`, `=> ${functionName} is invoked`))
+    const ctrl = this.app.controller(ctrlName);
+    const ctrlInstance: any = await ctrl.getValue(this.app)
+    await ctrlInstance[functionName]()
+  }
+
+  async fetchChickenData() {
     // Fetch records from database
     // const response = await this.customerInfoRepository.count()
     // this.logger.log('info', `Fetching data from repositories`);
-    const randRecordLen = Math.floor(Math.random() * (2000 - 700 + 100) + 100)
-    const rand = Math.floor(Math.random() * (300 - 100 + 10) + 10)
-    let response = []
+    const randRecordLen = Math.floor(Math.random() * (15 - 3) + 3)
+    let response: any = []
     for (let i = 0; i < randRecordLen; i++) {
       response.push({
         id: i,
         filename: `${this.name}-document-${i}`,
-        bitrate: rand
-      })
-    }
-    return response
-  }
-
-  async fetchForCorrect() {
-    // Fetch records from database
-    const randRecordLen = Math.floor(Math.random() * (2000 - 700 + 100) + 100)
-    const rand = Math.floor(Math.random() * (300 - 100 + 10) + 10)
-    let response = []
-    for (let i = 0; i < randRecordLen; i++) {
-      response.push({
-        id: i,
-        filename: `${this.name}-document-${i}`,
-        bitrate: rand
-      })
-    }
-    return response
-  }
-
-  async fetchRecordsToTransmit() {
-    const randRecordLen = Math.floor(Math.random() * (2000 - 700 + 100) + 100)
-    const rand = Math.floor(Math.random() * (300 - 100 + 10) + 10)
-    let response = []
-    for (let i = 0; i < randRecordLen; i++) {
-      response.push({
-        id: i,
-        filename: `${this.name}-document-${i}`,
-        bitrate: rand
-      })
-    }
-    return response
-  }
-
-  async fetchForUpdate() {
-    // Fetch records from database
-    const randRecordLen = Math.floor(Math.random() * (2000 - 700 + 100) + 100)
-    const rand = Math.floor(Math.random() * (300 - 100 + 10) + 10)
-    let response = []
-    for (let i = 0; i < randRecordLen; i++) {
-      response.push({
-        id: i,
-        filename: `${this.name}-document-${i}`,
-        bitrate: rand
-      })
-    }
-    return response
-  }
-
-  async fetchAging() {
-    // Fetch records from database
-    const randRecordLen = Math.floor(Math.random() * (2000 - 700 + 100) + 100)
-    const rand = Math.floor(Math.random() * (300 - 100 + 10) + 10)
-    let response = []
-    for (let i = 0; i < randRecordLen; i++) {
-      response.push({
-        id: i,
-        filename: `${this.name}-document-${i}`,
-        bitrate: rand
+        bitrate: Math.floor(Math.random() * (300 - 100) + 10)
       })
     }
     return response
   }
 
   async sendToProcessor() {
-    console.log(this.name, `Scheduler: Fetching data from repositories`);
+    console.log(italic.magentaBright('->', 'scheduler:sendToProcessor', '=> SendToProcesor has been called', this.name))
     try {
       switch (this.name) {
-        case 'linking':
-          let linkingResponse = await this.fetchRecordsToLink()
-          const linkingResponseInstance = new ProcessLinkingDocProvider(linkingResponse, this.name)
+        case 'dog':
+          let dogResponse = await this.invokeController(DogController, 'findDogs')
           break;
-        case 'transmission':
-          let transmissionResponse = await this.fetchRecordsToTransmit()
-          const transmissionResponseInstance = new ProcessTransmissionDocProvider(transmissionResponse, this.name)
+        case 'cat':
+          let catResponse = await this.invokeController(CatController, 'findCats')
           break;
-        case 'readForCorrect':
-          let forCorrectResponse = await this.fetchForCorrect()
-          const forCorrectResponseInstance = new ProcessForCorrectDocProvider(forCorrectResponse, this.name)
-          break;
-        case 'updateDoc':
-          let updateDocResponse = await this.fetchForUpdate()
-          const updateDocResponseInstance = new ProcessUpdateDocProvider(updateDocResponse, this.name)
-          break;
-        case 'aging':
-          let agingResponse = await this.fetchAging()
-          const agingResponseInstance = new ProcessAgingDocProvider(agingResponse, this.name)
+        case 'chicken':
+          let chickenResponse = await this.fetchChickenData()
+          const linkingResponseInstance = new ProcessChickenProvider(chickenResponse, this.name)
           break;
         default:
           break;
