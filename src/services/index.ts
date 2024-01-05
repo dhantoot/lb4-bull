54fbaffaa50b031deb5949b2a31f2bfc84c9690b
Dan Vincent Tagailo <danvincenttagailo@gmail.com>
2024-01-06 00:23:11 +0800

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,2 @@
-export * from './process-aging-doc.service';
-export * from './process-forCorrect-doc.service';
-export * from './process-linking-doc.service';
-export * from './process-transmission-doc.service';
-export * from './process-update-doc-service';
-
+export * from './processors/process-chicken.service';
 
